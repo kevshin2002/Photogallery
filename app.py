8d3a020ee7f9023e40907a4044303e198f788182
Kevin Shin <kevshin2002@gmail.com>
2024-02-24 15:32:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,53 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles       # Used for making static resources available to server
-from fastapi.templating import Jinja2Templates # Used for templatizied HTML
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 import uvicorn
 
-# Here, you're creating an instance of FastAPI. 
-# This is like starting up your FastAPI toolkit and getting it ready to use. 
-# We call this instance app, and it's what you'll use to create different parts of your web application.
-app = FastAPI() 
+app = FastAPI()
 
-# Specify where the static files are located. This is the name of your folder.
-static_files = StaticFiles(directory='public') 
+static_files = StaticFiles(directory='public')
 views = Jinja2Templates(directory="public/views")
 
-# Mount the static files directory to /public, this is where your other files will access the sources.
 app.mount('/public', static_files, name='public')
-
-# We modulate the different kinds of files so it's easier for us to navigate them
 app.mount("/imgs", StaticFiles(directory="public/imgs"), name="imgs")
 app.mount("/css", StaticFiles(directory="public/css"), name="css")
 app.mount("/js", StaticFiles(directory="public/js"), name="js")
 
+class Image(BaseModel):
+    title: str = None
+    description: str = None
+    tags: list[str] = []
+
+images = {}  # Stores images as {img_id: {"title": "example", "description": "desc", "tags": ["tag1", "tag2"]}}
+
 
 @app.get('/', response_class=HTMLResponse)
-def get_home() -> HTMLResponse:
+def get_home(request: Request) -> HTMLResponse:
     """
-    Get the homepage
-    :param request: the request object
-    :return: the homepage
+    Handles the getting of data, you should be passing the data with the main.html.
+
     """
-    return HTMLResponse(content=views.get_template("main.html").render(), status_code=200)
+    pass
 
 @app.post("/{img_id}")
-def post_image(img_src: str, img_id: int) -> str:
-    return(Auth.verify_availability(user.username, user.email))
+def post_image(img_id: int, img_data: Image):
+    """
+    Handles the posting of an image source URL associated with a given image ID.
+    :param img_id: The ID of the image to which the source URL is to be associated.
+    :param img_data: The image data received in the request body, including the source URL.
+    :return: A boolean value indicating success or failure of the operation.
+    """
+    pass
 
-@app.put('/{img_id}')
-def modify_image(img_id: int) -> dict:
-    session = sessions.get_session(request)
-    session['username'] = user.username
-    return {'success': Auth.update_user(user_id, user.first_name, user.last_name, user.email, user.username, Security.encrypt(user.password))}
+@app.put("/{img_id}")
+def modify_image(img_id: int, img_data: Image):
+    pass
 
-@app.delete('/customer/{img_id}')
-def delete_user(user_id:int) -> dict:
-    return {'success': Auth.delete_user(user_id)}
-
+@app.delete('/image/{img_id}')
+def delete_img(img_id: int):
+    pass
 
 if __name__ == "__main__":
-    # We can specify what ports to run, and you can change this.
-    # You would access this at 127.0.0.`:8007 or localhost:8007
-
-    # reload = True allows us to make changes to our code without constantly rerunning the code.
     uvicorn.run("app:app", host="127.0.0.1", port=8007, reload=True)
